82ee88a8101a6c8863faf155055614c83acff65e
DanielFengW <fengwendan@yeah.net>
2016-11-06 23:54:36 +0800

    Initial commit

---
+++ app/src/main/java/com/daniel/simpleweather/WeatherBean.java
@@ -0,0 +1,19 @@
+package com.daniel.simpleweather;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Generated;
+
+/**
+ *
+ * Created by Daniel Feng W on 2016/11/6.
+ */
+
+@Entity
+public class WeatherBean {
+
+	@Generated(hash = 2015408157)
+	public WeatherBean() {
+	}
+
+
+}
---
+++ app/src/main/java/com/daniel/simpleweather/service/WeatherService.java
@@ -0,0 +1,22 @@
+package com.daniel.simpleweather.service;
+
+import com.daniel.simpleweather.WeatherBean;
+import com.daniel.simpleweather.bean.WeatherResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by Daniel Feng W on 2016/11/6.
+ */
+
+public interface WeatherService {
+
+    @GET("data/sk/{id}.html")
+    Call<WeatherResponse> getWeatherResponse(@Path("id") String id);  //test id = 101010100
+
+
+
+}
